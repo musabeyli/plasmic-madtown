18f2f40d1a229447fb0947627f5d9e7f8aade331
musabeyli <musabeyli@wisc.edu>
2022-07-18 13:21:12 -0400

    Revert "undoing"
    This reverts commit 3d10dc73198c42729e5b3ea95b71eaa66714fc6c.

---
+++ .history/pages/api/checkout_sessions/index_20220716121617.ts
@@ -0,0 +1,82 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import Router, {useRouter} from 'next/router';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+export default async function handler(  
+    req: NextApiRequest,
+    res: NextApiResponse) {
+  if (req.method === 'POST') {
+    try {
+      // Create Checkout Sessions from body params.
+      stripe.checkout
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        shipping_address_collection: {
+          allowed_countries: ['US'],
+        },
+        shipping_options: [
+          {
+            shipping_rate_data: {
+              type: 'fixed_amount',
+              fixed_amount: {
+                amount: 0,
+                currency: 'usd',
+              },
+              display_name: 'Free shipping',
+              // Delivers between 5-7 business days
+              delivery_estimate: {
+                minimum: {
+                  unit: 'business_day',
+                  value: 5,
+                },
+                maximum: {
+                  unit: 'business_day',
+                  value: 7,
+                },
+              }
+            }
+          },
+          {
+            shipping_rate_data: {
+              type: 'fixed_amount',
+              fixed_amount: {
+                amount: 1500,
+                currency: 'usd',
+              },
+              display_name: 'Next day air',
+              // Delivers in exactly 1 business day
+              delivery_estimate: {
+                minimum: {
+                  unit: 'business_day',
+                  value: 1,
+                },
+                maximum: {
+                  unit: 'business_day',
+                  value: 1,
+                },
+              }
+            }
+          },
+        ],
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: 'price_1LMDMSLZ8TMriulmm899tlQ4',
+            quantity: req.body.amount,
+          },
+        ],
+        mode: 'payment',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      });
+      res.status(200).json(session);
+      // res.redirect(303, session.url);
+    } catch (err : any) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
---
+++ .history/pages/api/checkout_sessions/index_20220716121758.ts
@@ -0,0 +1,69 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import Router, {useRouter} from 'next/router';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+export default async function handler(  
+    req: NextApiRequest,
+    res: NextApiResponse) {
+  if (req.method === 'POST') {
+    try {
+      // Create Checkout Sessions from body params.
+      stripe.checkout
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        shipping_address_collection: {
+          allowed_countries: ['US'],
+        },
+        shipping_options: [
+          {
+            shipping_rate_data: {
+              type: 'fixed_amount',
+              fixed_amount: {
+                amount: 0,
+                currency: 'usd',
+              },
+              display_name: 'Free shipping',
+              // Delivers between 5-7 business days
+              delivery_estimate: {
+                minimum: {
+                  unit: 'business_day',
+                  value: 5,
+                },
+                maximum: {
+                  unit: 'business_day',
+                  value: 7,
+                },
+              }
+            }
+          },
+          
+        ],
+        line_items: [
+          {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: 'price_1LMDMSLZ8TMriulmm899tlQ4',
+            quantity: req.body.amount,
+          },
+        ],
+        mode: 'payment',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      });
+      res.status(200).json(session);
+      // res.redirect(303, session.url);
+    } catch (err : any) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
