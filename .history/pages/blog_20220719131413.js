23660155fe23c13b30f8031cfc819e276ba0f28d
musabeyli <musabeyli@wisc.edu>
2022-07-19 13:19:43 -0400

    commits

--- .history/pages/blog_20220719131413.js
+++ .history/pages/blog_20220719131413.js
@@ -4,7 +4,6 @@ import Layout from "../components/layout";
 import Seo from "../components/seo";
 import { fetchAPI } from "../lib/api";
 import { SocialProfileJsonLd } from "next-seo";
-import { NextSeo } from "next-seo";
 
 const Home = ({ articles, categories, homepage }) => {
   return (
