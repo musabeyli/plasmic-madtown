4bd806024fdfd072459c8df2e043b7b888b2cc02
musabeyli <musabeyli@wisc.edu>
2022-07-20 16:12:01 -0400

    Revert "commit tiktok"
    This reverts commit c5272fe633f9239a2b5db02180ab3c15d0ce2966.

--- next.config.js
+++ next.config.js
@@ -1,55 +1,8 @@
 /** @type {import('next').NextConfig} */
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://www.disqus.com https://www.youtu.be https://www.youtube.com https://www.google.com https://www.tiktok.com;
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
-  media-src 'none';
-  connect-src *;
-  font-src 'self' data;
-  frame-src giscus.app https://www.disqus.com https://www.youtu.be https://www.youtube.com https://www.google.com https://www.tiktok.com
-`;
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: ContentSecurityPolicy.replace(/\n/g, ""),
-  },
-  {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "SAMEORIGIN",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
-  },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=31536000; includeSubDomains; preload",
-  },
-  {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=()",
-  },
-];
-
 const nextConfig = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  },
-  async headers() {
-    return [
-      { source: "/product", headers: securityHeaders },
-      { source: "/:path*", headers: securityHeaders },
-    ];
   },
   images: {
     loader: "default",
