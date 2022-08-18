3950c888f231c99fc8fb4c8fc5e2f24fff84a70c
musabeyli <musabeyli@wisc.edu>
2022-08-18 07:54:12 -0400

    apply discount

--- pages/api/checkout_sessions/index.ts
+++ pages/api/checkout_sessions/index.ts
@@ -12,7 +12,7 @@ export default async function handler(
       stripe.checkout
           const session = await stripe.checkout.sessions.create({
           payment_method_types: ['card'],
-          allow_promotion_codes: true,
+          // allow_promotion_codes: true,
           shipping_address_collection: {
 			      allowed_countries: ['US']
 		      },
@@ -48,6 +48,9 @@ export default async function handler(
           },
         ],
       mode: 'payment',
+      discounts: [{
+        coupon: 'UWALUMNI',
+      }],
       success_url: `${req.headers.origin}/?success=true`,
       cancel_url: `${req.headers.origin}/?canceled=true`,
       });
